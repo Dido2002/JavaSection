1ccc3a34d4e9f77c51ad3947dcfe1905f2d2adc7
Dido <100509456+Dido2002@users.noreply.github.com>
2023-03-04 09:22:30 +0200

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = console.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println("Result = " + number * i);
+        }
+    }
+}
