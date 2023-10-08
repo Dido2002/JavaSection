d5ccfc74e2880f7e8076106fe08d527f05e6951d
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-08 19:43:56 +0300

    Add files via upload

---
+++ Java01.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+package com.mycompany.java01;
+import java.util.Random;
+
+/**
+ *
+ * @author PC
+ */
+public class Java01 {
+
+    public static void main(String[] args) {
+        //String x = "water";
+        //String y = "Kool-Aid";
+        //String temp;
+        
+        //temp = x;
+        //x=y;
+        //y=temp;
+        
+//        Scanner scanner =  new Scanner(System.in);
+//        System.out.println("Whats is your name?");
+//        
+//        String name = scanner.nextLine();
+//        
+//        System.out.println("How old are you?");
+//        int age = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.println("What is your favourite food?");
+//        String food = scanner.nextLine();
+//        System.out.println("Hello " + name);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("You like " + food);
+//        System.out.println("So your name is " + name + " you are " + age + " years old " + " and you like " + food);
+        
+
+//        String name = JOptionPane.showInputDialog("Enter your name");
+//        JOptionPane.showMessageDialog(null, "Hello "+ name);
+//        
+//        int age = Integer.parseInt( JOptionPane.showInputDialog("Enter your age"));
+//        JOptionPane.showMessageDialog(null, "You are "+ age + " years old");
+//
+//        
+//        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+//        JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
+        
+
+          
+//        double x;
+//        double y;
+//        double z;
+//        
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter side x: ");
+//        x = scanner.nextDouble();
+//        System.out.println("Enter side y: ");
+//        y = scanner.nextDouble();
+//        
+//        z = Math.sqrt((x*x)+(y*y));
+//        System.out.println("The hypotenuse is: " + z);
+//        
+//        scanner.close();
+
+            Random random = new Random();
+//            int x = random.nextInt(6)+1;
+//                double y = random.nextDoubles();
+                boolean z = random.nextBoolean();
+            System.out.println(z);
+    }
+}
