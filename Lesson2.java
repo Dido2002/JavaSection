9116ceab23a6c80249564d83915569c46aede827
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-08 19:41:00 +0300

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lesson2;
+
+import java.util.Scanner;
+/**
+ *
+ * @author PC
+ */
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        
+//        Scanner in = new Scanner("Enter a,b,c");
+//        double a = in.nextDouble();
+//        double b = in.nextDouble();
+//        double c = in.nextDouble();
+//        double d = b * b - 4 * a * c;
+//        if (d > 0) {
+//            double x1 = (-b + Math.sqrt(d)) / 2 * a;
+//            double x2 = (-b - Math.sqrt(d)) / 2 * a;
+//            System.out.println("x1= " + x1 + " x2 = " + x2);
+//
+//        } else if (d == 0) {
+//            System.out.println("Nqma realni koreni");
+//
+//        } else {
+//            double x = -b / 2 * a;
+//            System.out.println("x= " + x);
+//        }
+
+            
+        
+    }
+}
